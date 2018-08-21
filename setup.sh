d6d6a4fdf470ba52a50bca1437739636c51caf01
mikik@nttdata.co.jp <mikik@nttdata.co.jp>
2018-08-21 11:39:29 +0900

    setup.sh追加

--- setup.sh
+++ setup.sh
@@ -1,14 +1,8 @@
 #!/bin/sh
 
 VIMRC=~/.vimrc
-SCREENRC=~/.screenrc
-TMUX_DIR=~/.tmux
-TMUX_CONF=~/.tmux.conf
-TMUX_RC=~/.tmux-powerlinerc
 ZSHRC=~/.zshrc
 ZSH_PROFILE=~/.zprofile
-CACHE_DIR=~/.cache
-VIM_DEIN_DIR=$CACHE_DIR/dein
 DEIN_TOML=~/.dein.toml
 DEIN_LAZY_TOML=~/.dein_lazy.toml
 TIGRC=~/.tigrc
@@ -40,7 +34,7 @@ backup()
 # main
 
 # set backup list 
-CHECK_TARGET="$VIMRC $SCREENRC $TMUX_DIR $TMUX_CONF $BASHRC $BASH_PROFILE $CACHE_DIR $DEIN_TOML $DEIN_LAZY_TOML $TIGRC"
+CHECK_TARGET="$VIMRC $ZSHRC $ZPROFILE $DEIN_TOML $DEIN_LAZY_TOML $TIGRC"
 
 # backup
 for target in ${CHECK_TARGET[@]}
@@ -48,19 +42,10 @@ do
   backup $target
 done
 
-# create dein directory
-mkdir -p $VIM_DEIN_DIR
-
-# install tmux-powerline
-git clone https://github.com/takkumattsu/tmux-powerline.git $TMUX_DIR/tmux-powerline
-
 # cretae symbolic link
 ln -s $PWD/.vimrc $VIMRC
-ln -s $PWD/.screenrc $SCREENRC
-ln -s $PWD/.tmux.conf $TMUX_CONF
-ln -s $PWD/.bashrc $BASHRC
-ln -s $PWD/.bash_profile $BASH_PROFILE
+ln -s $PWD/.zshrc $ZSHRC
+ln -s $PWD/.zprofile $ZPROFILE
 ln -s $PWD/.dein.toml $DEIN_TOML
 ln -s $PWD/.dein_lazy.toml $DEIN_LAZY_TOML
 ln -s $PWD/.tigrc $TIGRC
-ln -s $PWD/.tmux-powerlinerc $TMUX_RC
